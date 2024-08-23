b7545a8e5686b9a6e8be5d174cb8460cfd47d08b
Its-Maniaco <21123621+Its-Maniaco@users.noreply.github.com>
2024-08-23 15:47:50 +0530

    fixed css

--- src/home.js
+++ src/home.js
@@ -1,17 +1,22 @@
+import resturantImage from "../public/img/resturant.jpg";
+
 export default function Home() {
     const content = document.getElementById("content");
 
-    const heading = document.createElement("h1");
-    heading.content = "Welcome to The FooD Resturant";
+    content.innerHTML = '';
 
-    const resturantImage = document.createElement("img");
-    resturantImage.src = "../public/img/resturant.jpg";
-    resturantImage.alt = "Image to resturant";
+    const heading = document.createElement("h1");
+    heading.textContent = "Welcome to The FooD Resturant";
+    content.appendChild(heading);
+    
+    const resturantPhoto = document.createElement("img");
+    resturantPhoto.src = resturantImage;
+    resturantPhoto.alt = "Image to resturant";
 
     const description = document.createElement('p');
     description.textContent = "Come eat FooD at FooD Resturant."
 
-    content.appendChild(heading);
-    content.appendChild(resturantImage);
+    
+    content.appendChild(resturantPhoto);
     content.appendChild(description);
-}
+} 
--- src/index.js
+++ src/index.js
@@ -2,6 +2,9 @@ import { About } from "./about";
 import Home from "./home";
 import { Menu } from "./menu";
 
+import "./styles.css";
+
+//TODO: add clear content to remove old content which will be replaced.
 document.getElementById("home").addEventListener("click", () => {
     Home();
 })
